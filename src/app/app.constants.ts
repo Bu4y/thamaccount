ca6e480667308b0cce1daaf668d6f2c0a8d8c55b
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-08-08 15:43:21 +0700

    login signup

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 export class Constants {
-  public static get URL(): string { return "https://localhost:3000/"; };
+  // public static get URL(): string { return "https://localhost:3000/"; };
+  public static get URL(): string { return "https://mbjenterprise-admin.herokuapp.com/"; };
 }
