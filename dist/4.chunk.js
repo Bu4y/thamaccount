5479a65128ab27de981d897f4a7b2553aad9efe7
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-08-10 21:54:21 +0700

    add account

---
+++ dist/4.chunk.js
@@ -0,0 +1,271 @@
+webpackJsonp([4],{
+
+/***/ "../../../../../src/app/layout/account/account-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__account_component__ = __webpack_require__("../../../../../src/app/layout/account/account.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AccountRoutingModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_2__account_component__["a" /* AccountComponent */] }
+];
+var AccountRoutingModule = (function () {
+    function AccountRoutingModule() {
+    }
+    return AccountRoutingModule;
+}());
+AccountRoutingModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forChild(routes)],
+        exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */]]
+    })
+], AccountRoutingModule);
+
+//# sourceMappingURL=account-routing.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/account/account.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n    <ol class=\"breadcrumb\">\r\n        <li class=\"breadcrumb-item active\"><i class=\"fa fa-list-ul\"></i> Account</li>\r\n    </ol>\r\n    <div class=\"row\">\r\n        <div class=\"col-12 text-center\">\r\n            <button type=\"submit\" class=\"btn btn-primary\">New Account</button>\r\n        </div>\r\n    </div>\r\n    <div class=\"row\">\r\n        <div class=\"col-12\">\r\n            <div class=\"ui-tree-list\">\r\n                <ui-tree [data]=\"data\" [key]=\"key\"></ui-tree>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>\r\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/account/account.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".ui-tree-list {\n  margin-left: -30px !important;\n  padding-top: 30px; }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/account/account.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AccountComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AccountComponent = (function () {
+    function AccountComponent() {
+        this.key = 'parent';
+        this.data = [{
+                name: "สินทรัพย์",
+                accountno: 1000000,
+                status: "active",
+                parent: [{
+                        name: "สินทรัพย์1",
+                        accountno: 1100000,
+                        parent: [],
+                        status: "active"
+                    }],
+            }, {
+                name: "หนี้สิน",
+                accountno: 2000000,
+                parent: [{
+                        name: "หนี้สิน1",
+                        accountno: 2100000,
+                        status: "active",
+                        parent: [{
+                                name: "หนี้สิน2",
+                                accountno: 2110000,
+                                parent: [{
+                                        name: "หนี้สิน3",
+                                        accountno: 2111000,
+                                        parent: [],
+                                        status: "active"
+                                    }],
+                                status: "active"
+                            }],
+                    }],
+                status: "active"
+            }];
+        this.accounts = [{
+                name: "สินทรัพย์",
+                accountno: 1000000,
+                parent: null,
+                status: "active",
+            },
+            {
+                name: "test",
+                accountno: 1100000,
+                parent: 1000000,
+                status: "active"
+            },
+            {
+                name: "หนี้สิน",
+                accountno: 2000000,
+                parent: null,
+                status: "active"
+            },
+            {
+                name: "test2",
+                accountno: 2100000,
+                parent: 2000000,
+                status: "active"
+            },
+            {
+                name: "test3",
+                accountno: 2110000,
+                parent: 2100000,
+                status: "active"
+            }];
+        this.showMenu = '';
+    }
+    AccountComponent.prototype.ngOnInit = function () {
+    };
+    AccountComponent.prototype.addExpandClass = function (item) {
+        item.expand = item.expand ? false : true;
+    };
+    return AccountComponent;
+}());
+AccountComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-account',
+        template: __webpack_require__("../../../../../src/app/layout/account/account.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/layout/account/account.component.scss")]
+    }),
+    __metadata("design:paramtypes", [])
+], AccountComponent);
+
+//# sourceMappingURL=account.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/account/account.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__account_component__ = __webpack_require__("../../../../../src/app/layout/account/account.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__account_routing_module__ = __webpack_require__("../../../../../src/app/layout/account/account-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__shared_components_ui_tree_ui_tree__ = __webpack_require__("../../../../../src/app/shared/components/ui-tree/ui-tree.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AccountModule", function() { return AccountModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AccountModule = (function () {
+    function AccountModule() {
+    }
+    return AccountModule;
+}());
+AccountModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["k" /* CommonModule */],
+            __WEBPACK_IMPORTED_MODULE_3__account_routing_module__["a" /* AccountRoutingModule */]
+        ],
+        declarations: [__WEBPACK_IMPORTED_MODULE_2__account_component__["a" /* AccountComponent */], __WEBPACK_IMPORTED_MODULE_4__shared_components_ui_tree_ui_tree__["a" /* UiTree */]]
+    })
+], AccountModule);
+
+//# sourceMappingURL=account.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/components/ui-tree/ui-tree.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".just-padding {\n  padding-left: 30px; }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/components/ui-tree/ui-tree.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return UiTree; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var UiTree = (function () {
+    function UiTree() {
+    }
+    UiTree.prototype.clickItem = function (item) {
+        window.event.stopPropagation();
+        item.expand = item.expand ? false : true;
+    };
+    return UiTree;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Input"])('data'),
+    __metadata("design:type", Object)
+], UiTree.prototype, "items", void 0);
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Input"])('key'),
+    __metadata("design:type", String)
+], UiTree.prototype, "key", void 0);
+UiTree = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: "ui-tree",
+        template: "\n    <div class=\"just-padding\" *ngIf=\"items.length\">\n      <div class=\"list-group list-group-root\"  class=\"main-list\" *ngFor=\"let item of items\" (click)=\"clickItem(item)\">\n        <a class=\"list-group-item list-group-item-action\">\n          <i class=\"fa fa-caret-right\" *ngIf=\"!item.expand && item[key].length\"></i>\n          <i class=\"fa fa-caret-down\" *ngIf=\"item.expand && item[key].length\"></i>\n          &nbsp;\n          {{item.accountno}}\n          &nbsp;\n          {{item.name}} \n        </a>\n          <ui-tree *ngIf=\"item[key].length && item.expand == true\" [key]=\"key\" [data]=\"item[key]\"></ui-tree>\n      </div>\n    </div>\n  ",
+        styles: [__webpack_require__("../../../../../src/app/shared/components/ui-tree/ui-tree.scss")]
+    })
+], UiTree);
+
+//# sourceMappingURL=ui-tree.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=4.chunk.js.map
